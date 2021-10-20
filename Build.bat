ff57f6615dde9cc4161f4e4dd79c70079255e562
TsunayoshiSawada <animekaizokux@gmail.com>
2021-10-20 21:00:02 +0530

    Edits to build bat

--- Build.bat
+++ Build.bat
@@ -3,7 +3,7 @@ TITLE Building a binary
 
 cargo build --release
 nssm stop PsychoPass >nul
-move E:\PsychoPass\target\release\SpamWatchAPI.exe E:\PsychoPass\
+move E:\PsychoPass\target\release\PsychoPass.exe E:\PsychoPass\
 nssm start PsychoPass >nul
 echo All done! check if any errors exist!
 timeout 5
